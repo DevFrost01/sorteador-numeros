3524ff782cbd04619c25bdede74b60a630908b30
Frost <devfrost0101@gmail.com>
2024-02-19 15:01:20 -0300

    Main

---
+++ app.js
@@ -0,0 +1,15 @@
+function sortear() {
+
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+
+    alert(`Quantidade de números: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até: ${ate}`);
+}
+
+function obterNumeroAleatorio(min, max) {
+    
+}
