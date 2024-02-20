2a17f4fbf291c26d72c8e5e20a056e71a6e44d17
Frost <devfrost0101@gmail.com>
2024-02-20 11:55:54 -0300

    removendo loop infitnito

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ function sortear() {
         return;
     } 
 
+    if (quantidade > (ate - de + 1)) {
+        alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+        return;
+    }
+    
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
 
